f71a41fbef28506f7939a042f9f0c532e71bac23
Christ Sadi <christsadi@gmail.com>
2022-09-26 09:37:09 +0200

    implement ContactsPage as a stateful component

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { contactForm } from "../../components/contactForm/ContactForm";
+import { TileList, tileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({contacts, addContacts}) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+ const [ name, setName ] = useState("");
+ const [ email, setEmail ] = useState("");
+ const [ phone, setPhone ] = useState("");
+ const [ duplicate, setDuplicate] = useState(false);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,12 +19,36 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if (!duplicate){
+    addContacts(name, email, phone);
+    setName("");
+    setEmail("");
+    setPhone("");
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(
+    () => {
+      const nameIsDuplicate = () => {
+        const found = contacts.find((contact) => contact.name === name);
+
+        if (found !== undefined) {
+          return true;
+        } else 
+        return false;
+      };
+
+      if (nameIsDuplicate()) {
+        setDuplicate(true)
+      } else 
+       setDuplicate(false)
+    },
+    [name, contacts, duplicate]
+   );
 
   return (
     <div>
@@ -27,6 +58,7 @@ export const ContactsPage = () => {
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts/>
       </section>
     </div>
   );
