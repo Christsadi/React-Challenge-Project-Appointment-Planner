03de9203c30d0f2cdc057390893f32eff61af296
Christ Sadi <christsadi@gmail.com>
2022-09-26 10:30:45 +0200

    implement a stateless component to render a form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,24 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form  onSubmit={()=>{}}>
+      <label>
+        <input value={name} type='text' onChange={(e)=> setName(e.target.value) } 
+        required placeholder='Contact Name' />
+      </label>
+      <br/>
+      <label>
+        <input value={email} type='email' onChange={(e)=> setEmail(e.target.value) } 
+        required placeholder='Email address'/>
+      </label>
+      <br/>
+      <label>
+        <input value={phone} pattern={"[0][1-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"} 
+        type='tel' onChange={(e)=> setPhone(e.target.value) } 
+        required placeholder='Contact Number' />
+      </label> 
+      <button type='submit' />
+    </form>
+    
   );
 };
