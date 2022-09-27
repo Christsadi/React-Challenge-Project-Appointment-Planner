aa4073461b57b99f8ff167419bba0f745573613c
Christ Sadi <christsadi@gmail.com>
2022-09-27 08:25:05 +0200

    implement a stateless component to renders data

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({tiles}) => {
+
   return (
     <div className="tile-container">
-      
+      {
+        Object.values(tiles).map(
+          (value, idx) => (
+           <p key={value} className={`${idx === 0 ? "title-title" : ""} tile`} > {value}</p> 
+          )
+        )
+      }
     </div>
   );
 };
