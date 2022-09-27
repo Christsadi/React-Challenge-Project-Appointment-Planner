12a18a917cb598dd4718f62575a466755a3601b4
Christ Sadi <christsadi@gmail.com>
2022-09-27 09:42:36 +0200

    implement a stateless component to renders a form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ContactPicker from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -19,7 +20,30 @@ export const AppointmentForm = ({
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
+  const getContactName = ()=>{
+    return contacts.map((contact) => contact.name)
+  };
+
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit} >
+      <label>
+        <input value={title} type='text' required placeholder='Appointment Title' onChange={(e)=> setTitle(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        <input value={date} type='date' onChange={(e)=> setDate(e.target.value) } required min={getTodayString}/>
+      </label>
+      <br />
+      <label>
+        <input value={time} type='time' required onChange={(e)=> setTime(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        <ContactPicker contacts={getContactName} name='contact' placeholder='Appointment with' value={contact} onChange={(e)=> setContact(e.target.value)} />
+      </label>
+      
+      <button type='submit' value=' Add Appointment' />
+
+    </form>
   );
 };
