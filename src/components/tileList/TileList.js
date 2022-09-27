a28b31329b7b8e280d11f0ef6b6b97f0886f0c36
Christ Sadi <christsadi@gmail.com>
2022-09-27 08:24:27 +0200

    implement a stateless component to renders a list

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({tiles}) => {
+
   return (
     <div>
+      {tiles.map((tile, idx)=>(
+         <Tile ile={tile} key={idx}/>
+      )  
+      )}
       
     </div>
   );
