923049be218f17ebed3a4a6ba75e16072dcf284c
Christ Sadi <christsadi@gmail.com>
2022-09-27 08:26:16 +0200

    implement a stateful component to handle logic

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+
+export const AppointmentsPage = ({contacts, addApointment, appointments}) => {
   /*
   Define state variables for 
   appointment info
   */
+ const [title, setTitle] = useState ('');
+ const [contact, setContact] = useState (contacts.length > 0 ? contacts[0].name :'');
+ const [date, setDate] = useState ('');
+ const [time, setTime] = useState ('');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+   addApointment(title, contact, date, time);
+   setTitle('');
+   setDate('');
+   setTime('');
+   setContact('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+        title={title} 
+        setTitle={setTitle} 
+        contact={contact} 
+        setContact={setContact} 
+        date={date}
+        setDate={setDate}
+        time={time}
+        setTime={setTime}
+        handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments}/>
       </section>
     </div>
   );
